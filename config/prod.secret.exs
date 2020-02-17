54be80392862a6a7b32e65be2314d8e5512a303e
Michael Dalton <modintexas@gmail.com>
2020-02-16 23:33:03 -0600

    Start webserver on release

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -32,7 +32,7 @@ config :homecooked, HomecookedWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :homecooked, HomecookedWeb.Endpoint, server: true
+    config :homecooked, HomecookedWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
