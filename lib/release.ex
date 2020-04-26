16cb32d2610e1551ecfcece7caa71a9b668ce550
Michael Dalton <modintexas@gmail.com>
2020-04-26 01:44:44 -0700

    Attempted rollback

--- lib/release.ex
+++ lib/release.ex
@@ -7,8 +7,16 @@ defmodule Homecooked.Release do
     end
   end
 
-  def rollback(repo, version) do
-    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  def rollback(version) do
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+    end
+  end
+
+  def list_migrations() do
+    for repo <- repos() do
+      IO.inspect(Ecto.Migrator.migrations(repo))
+    end
   end
 
   defp repos do
